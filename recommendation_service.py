020cc47bb7bc512f87b728eeb6a85878c4d9f6ae
Manaam <manaam@yandex.ru>
2026-03-12 21:58:12 +0000

    оценка рекоммендаций

--- recommendation_service.py
+++ recommendation_service.py
@@ -124,3 +124,35 @@ async def recommendations_online(user_id: int, k: int = 100):
     recs = dedup_ids(combined)
 
     return {"recs": recs[:k]}
+
+
+@app.post("/recommendations")
+async def recommendations(user_id: int, k: int = 100):
+    """
+    Возвращает список рекомендаций длиной k для пользователя user_id
+    """
+
+    recs_offline = await recommendations_offline(user_id, k)
+    recs_online = await recommendations_online(user_id, k)
+
+    recs_offline = recs_offline["recs"]
+    recs_online = recs_online["recs"]
+
+    recs_blended = []
+
+    min_length = min(len(recs_offline), len(recs_online))
+    # чередуем элементы из списков, пока позволяет минимальная длина
+    for i in range(min_length):
+        recs_blended.append(recs_online[i])
+        recs_blended.append(recs_offline[i])
+
+    # добавляем оставшиеся элементы в конец
+    recs_blended += recs_online[min_length:] + recs_offline[min_length:]
+
+    # удаляем дубликаты
+    recs_blended = dedup_ids(recs_blended)
+
+    # оставляем только первые k рекомендаций
+    recs_blended = recs_blended[:k]
+
+    return {"recs": recs_blended}
